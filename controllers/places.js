fff6966527365c94e46226a7ebc4b19514b09630
Karon Oquendo <Keoquendo0308@gmail.com>
2024-04-24 16:14:20 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -3,7 +3,11 @@ const places = require("../models/places")
 
 
 router.get('/', (req, res) => {
-    res.render('places/index', { places})
+    res.render('places/index', { places })
+})
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
 })
 
 
