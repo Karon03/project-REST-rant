540b63c99bdc18996055463ee6c8e633385e55a4
Karon Oquendo <Keoquendo0308@gmail.com>
2024-09-12 11:49:54 -0400

    Stars displayed for ratings, put/delete routes created

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
+const comment = require('../models/comment')
 
 router.get('/', (req, res) => {
     db.Place.find()
@@ -12,20 +13,36 @@ router.get('/', (req, res) => {
         })
 })
 
-
-
 router.post('/', (req, res) => {
+    console.log(req.body);
     db.Place.create(req.body)
         .then(() => {
             res.redirect('/places')
         })
         .catch(err => {
-            console.log('err', err)
-            res.render('error404')
+            if (err && err.name == 'ValidationError') {
+                let message = "Validation Errors: ";
+
+                if (err && err.name == 'ValidationError') {
+                    let message = 'Validation Error: '
+                    for (var field in err.errors) {
+                        message += ` ${field} was ${err.errors[field].value}. `
+                        message += `${err.errors[field].message}`
+                    }
+                    console.log('Validation error message', message)
+                    res.render('places/new', { message })
+                }
+                else {
+                    res.render('error404')
+                }
+
+                res.render('places/new', { message });
+            }
+            else {
+                res.render('error404')
+            }
         })
 })
-
-
 
 router.get('/new', (req, res) => {
     res.render('places/new')
@@ -44,7 +61,6 @@ router.get('/:id', (req, res) => {
         })
 })
 
-
 router.get('/:id/comment', (req, res) => {
     console.log(req.body);
     db.Place.findById(req.params.id)
@@ -56,16 +72,17 @@ router.get('/:id/comment', (req, res) => {
 router.post('/:id/comment', (req, res) => {
     console.log("!!!!!!!!!!!!!!")
     console.log(req.body)
-    if (req.body.rant = "on") {
-        req.body.rant = true
-    } else {
-        req.body.rant = false
-    }
+    const isRant = req.body.rant === 'on';
     db.Place.findById(req.params.id)
         .then(place => {
             console.log("Place to add comment:")
             console.log(place)
-            db.Comment.create(req.body)
+            db.Comment.create({
+                content: req.body.content,
+                author: req.body.author,
+                stars: req.body.stars,
+                rant: isRant
+            })
                 .then(comment => {
                     console.log("New comment:")
                     console.log(comment)
@@ -86,24 +103,58 @@ router.post('/:id/comment', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-    res.send('PUT /places/:id stub')
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 router.delete('/:id', (req, res) => {
-    res.send('DELETE /places/:id stub')
 })
 
+
 router.get('/:id/edit', (req, res) => {
-    res.send('GET edit form stub')
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/edit', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
+
 
 router.post('/:id/rant', (req, res) => {
     res.send('GET /places/:id/rant stub')
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.delete('/:id', (req, res) => {
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(place => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
-module.exports = router
+router.delete('/:placeId/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(comment => {
 
+            res.redirect('/places/' + req.params.placeId)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+
+
+module.exports = router
